6039651ed2307a7fa72df29766665f2a687419fd
Brian Shirai <brixen@gmail.com>
2013-04-14 17:14:51 -0700

    Created gem.

---
+++ lib/rubysl-open_struct.rb
@@ -0,0 +1,7 @@
+require "rubysl-open_struct/version"
+
+module RubySL
+  module OpenStruct
+    # Your code goes here...
+  end
+end
---
+++ lib/rubysl-open_struct/version.rb
@@ -0,0 +1,5 @@
+module RubySL
+  module OpenStruct
+    VERSION = "0.0.1"
+  end
+end
